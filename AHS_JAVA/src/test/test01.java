d181b4661303b25de316246ec57e89dd4019a214
Administrator <Administrator@20180604-123207>
2020-12-09 13:23:38 +0900

    test01 3의 배수인지 아닌지 알아내는 코드입니다

---
+++ AHS_JAVA/src/test/test01.java
@@ -0,0 +1,16 @@
+package test;
+
+import java.util.Scanner;
+
+public class test01 {public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	int num = sc.nextInt();
+	if (num % 3 == 0) {
+		System.out.println("3의 배수입니다");
+	}
+	else {
+		System.out.println("3의 배수가 아닙니다");
+	}
+  }
+}
+
