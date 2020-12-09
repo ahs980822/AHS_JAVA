db38a75bd1268a431fe3935dbe74c69ac3758f5d
Administrator <Administrator@20180604-123207>
2020-12-09 13:54:44 +0900

    test04
    치킨메뉴판을 통해 번호를 입력하면 ~가 주문되었습니다 라는 코드를 만들어보았습니다.

--- AHS_JAVA/src/test/test04.java
+++ AHS_JAVA/src/test/test04.java
@@ -1,27 +1,68 @@
 package test;
 
+import java.util.Scanner;
+
 public class test04 {
+	
+	// 메소드 정의
+	// [접근지정자] (static) [반환타입] [메소드명]( [매개변수] ) {}
+	public static void showMenu() {
+		System.out.println("########## 치킨 메뉴판 ##########");
+		System.out.println("1. 뿌링클 치킨");
+		System.out.println("2. 마라 치킨");
+		System.out.println("3. 오리지널 치킨");
+		System.out.println("0. (종료)");
+		System.out.print("########## 입력 : ");
+	}
+	
 	public static void main(String[] args) {
-		// 베열 선언 및 초기화
-		// 배열의 크기 : 7  -> week.length 
-		String week[] = {"월","화","수","목","금","토","일"};
+		// (필요한 변수) 	: (메뉴번호), (메뉴)
+		// (메뉴판 메소드)  : showMenu()
+		Scanner sc = new Scanner(System.in);
+		int num = 0;
+		String menu = "";
 		
-		// 반복문
-		// index : 반복의 순서를 나타내는 순서번호
-		// for( ①[초기식] ; ②[조건식] ; ④[증감식] ) {③}
-		// ① > 반복( ② > ③ > ④ )
-		// i++;   ::   i = i + 1;
-		for (int i = 0; i < week.length; i++) {
-			System.out.print(week[i] + "요일 ");
-		}
+		// do~while
+		// 무조건 1번은 실행 후, 조건을 확인
+		do {
+			// 1. 메뉴판 출력
+			//    메소드 호출
+			showMenu();
+			
+			// 2. 메뉴번호 입력
+			num = sc.nextInt();
+			
+			// 3. 메뉴 주문
+			//    0 	: 종료
+			//    1,2,3	: [메뉴] + 주문이 완료되었습니다. 
+			//    etc   : (0~3) 사이의 숫자를 입력해주세요.
+			if( num == 0 )
+				break;			// 반복문 탈출
+			
+			switch (num) {
+				case 1: 
+						menu = "뿌링클 치킨";
+						break;
+				case 2: 
+						menu = "마라 치킨";
+						break;
+				case 3: 
+						menu = "오리지널 치킨";
+						break;
+
+				default:
+						break;
+			}
+			
+			if( num <= 3 )
+				System.out.println("\'" + menu + "\'" + "이(/가) 주문되었습니다!!! \n");
+			else
+				System.out.println("(0~3) 사이의 숫자를 입력해주세요. \n");
+			
+			
+		} while( true );
 		
-		System.out.println();
 		
-		// for-each(Advanced for)
-		// 자료구조의 모든 요소를 순회
-		for (String str : week) {
-			System.out.print(str + "요일 ");		// str == week[i]
-		}
 		
 	}
 }
